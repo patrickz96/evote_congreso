0e1a180e7f56cf887da356968c1316db0ca2f4fa
Your Name <you@example.com>
2020-05-31 00:19:21 -0500

    .gitignore Fixed

---
+++ config/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    'secret': 'secret-ouis0101'
+};
+
+/*
+
+"development": {
+    "username": "nnezeqdygiubyg",
+    "password": "e3f61d1aeff561d1c4019d473d2bbd007fe0e097d62c6a5844383acdb796713a",
+    "database": "daitvf87a4l2mq",
+    "host": "ec2-52-71-55-81.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "operatorsAliases": false,
+    "dialectOptions": {
+      "native": true,
+      "ssl": true
+    },
+    "define": {
+      "timestamps": false
+    }
+  }
+
+  "development": {
+    "username": "postgres",
+    "password": "xda",
+    "database": "evote",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "define": {
+      "timestamps": false
+    }
+  },
+
+*/
